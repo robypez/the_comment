d1d928193e0b365c0dd5e759230155b867205056
Roberto Pezzali <scrivoaroby@gmail.com>
2024-03-18 22:12:59 +0100

    mod

--- lib/the_comments.rb
+++ lib/the_comments.rb
@@ -20,19 +20,3 @@ end
 _root_ = File.expand_path('../../',  __FILE__)
 require "#{_root_}/config/routes.rb"
 
-if StateMachine::VERSION.to_f <= 1.2
-  puts '~' * 50
-  puts 'TheComments'
-  puts '~' * 50
-  puts 'WARNING!'
-  puts 'StateMachine patch for Rails4 will be applied'
-  puts
-  puts '> private method *around_validation* from StateMachine::Integrations::ActiveModel will be public'
-  puts
-  puts 'https://github.com/pluginaweek/state_machine/issues/295'
-  puts 'https://github.com/pluginaweek/state_machine/issues/251'
-  puts '~' * 50
-  module StateMachine::Integrations::ActiveModel
-    public :around_validation
-  end
-end
